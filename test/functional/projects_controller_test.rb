fff390fb3f910d64b3862f4fac2252722504cb45
Fabio Kreusch <fabiokr@gmail.com>
2009-07-21 10:20:44 -0300

    Fixed spacement

--- test/functional/projects_controller_test.rb
+++ test/functional/projects_controller_test.rb
@@ -2,117 +2,116 @@ require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
 
-    context 'on GET to :index' do
-      setup do
-        @products = [Factory(:product), Factory(:product)].paginate
-        @search = Product.new_search
+  context 'on GET to :index' do
+    setup do
+      @products = [Factory(:product), Factory(:product)].paginate
+      @search = Product.new_search
 
-        @search.stubs(:all => @products, :count => @products.size)
-        Product.expects(:new_search => @search)
+      @search.stubs(:all => @products, :count => @products.size)
+      Product.expects(:new_search => @search)
 
-        get :index
-      end
-
-      should_respond_with :success
-      should_assign_to(:search) {@search}
-      should_assign_to(:products) {@products}
+      get :index
     end
 
-    context "on GET to :new" do
-      setup do
-        get :new
-      end
+    should_respond_with :success
+    should_assign_to(:search) {@search}
+    should_assign_to(:products) {@products}
+  end
 
-      should_respond_with :success
+  context "on GET to :new" do
+    setup do
+      get :new
     end
 
-    context "on POST to :create" do
-      setup do
-        @product = Factory(:product)
-        Product.expects(:new => @product)
-        @product.expects(:save => true)
+    should_respond_with :success
+  end
 
-        post :create, :product => @product.attributes
-      end
+  context "on POST to :create" do
+    setup do
+      @product = Factory(:product)
+      Product.expects(:new => @product)
+      @product.expects(:save => true)
 
-      should_redirect_to('show') {product_path(@product)}
-      should_set_the_flash_to I18n.t('system.successfully_created', :model => I18n.t('activerecord.models.product'))
+      post :create, :product => @product.attributes
     end
 
-    context "on POST to :create with invalid records" do
-      setup do
-        @product = Factory(:product)
-        Product.expects(:new => @product)
-        @product.expects(:save => false)
-        mock_record_invalid(@product)
+    should_redirect_to('show') {product_path(@product)}
+    should_set_the_flash_to I18n.t('system.successfully_created', :model => I18n.t('activerecord.models.product'))
+  end
 
-        post :create, :product => @product.attributes
-      end
+  context "on POST to :create with invalid records" do
+    setup do
+      @product = Factory(:product)
+      Product.expects(:new => @product)
+      @product.expects(:save => false)
+      mock_record_invalid(@product)
 
-      should_render_template :new
+      post :create, :product => @product.attributes
     end
 
-    context "on GET to :show" do
-      setup do
-        @product = Factory(:product)
-        Product.expects(:find => @product)
+    should_render_template :new
+  end
 
-        get :show, :id => @product.id
-      end
+  context "on GET to :show" do
+    setup do
+      @product = Factory(:product)
+      Product.expects(:find => @product)
 
-      should_respond_with :success
-      should_assign_to(:product) {@product}
+      get :show, :id => @product.id
     end
 
-    context "on GET to :edit" do
-      setup do
-        @product = Factory(:product)
-        Product.expects(:find => @product)
+    should_respond_with :success
+    should_assign_to(:product) {@product}
+  end
 
-        get :edit, :id => @product.id
-      end
+  context "on GET to :edit" do
+    setup do
+      @product = Factory(:product)
+      Product.expects(:find => @product)
 
-      should_respond_with :success
-      should_assign_to(:product) {@product}
+      get :edit, :id => @product.id
     end
 
-    context "on POST to :update" do
-      setup do
-        @product = Factory(:product)
-        Product.expects(:find => @product)
-        @product.expects(:update_attributes => true)
+    should_respond_with :success
+    should_assign_to(:product) {@product}
+  end
 
-        post :update, :id => @product.id, :product => @product.attributes
-      end
+  context "on POST to :update" do
+    setup do
+      @product = Factory(:product)
+      Product.expects(:find => @product)
+      @product.expects(:update_attributes => true)
 
-      should_redirect_to('show') {product_path(@product)}
-      should_set_the_flash_to I18n.t('system.successfully_updated', :model => I18n.t('activerecord.models.product'))
+      post :update, :id => @product.id, :product => @product.attributes
     end
 
-    context "on POST to :update with invalid records" do
-      setup do
-        @product = Factory(:product)
-        Product.expects(:find => @product)
-        @product.expects(:update_attributes => false)
+    should_redirect_to('show') {product_path(@product)}
+    should_set_the_flash_to I18n.t('system.successfully_updated', :model => I18n.t('activerecord.models.product'))
+  end
 
-        post :update, :id => @product.id, :product => @product.attributes
-      end
+  context "on POST to :update with invalid records" do
+    setup do
+      @product = Factory(:product)
+      Product.expects(:find => @product)
+      @product.expects(:update_attributes => false)
 
-      should_render_template :edit
+      post :update, :id => @product.id, :product => @product.attributes
     end
 
-    context "on DELETE to :destroy" do
-      setup do
-        @product = Factory(:product)
-        Product.expects(:find => @product)
-        @product.expects(:destroy => true)
+    should_render_template :edit
+  end
 
-        delete :destroy, :id => @product.id
-      end
+  context "on DELETE to :destroy" do
+    setup do
+      @product = Factory(:product)
+      Product.expects(:find => @product)
+      @product.expects(:destroy => true)
 
-      should_redirect_to('index') {products_path}
-      should_set_the_flash_to I18n.t('system.successfully_destroyed', :model => I18n.t('activerecord.models.product'))
+      delete :destroy, :id => @product.id
     end
 
+    should_redirect_to('index') {products_path}
+    should_set_the_flash_to I18n.t('system.successfully_destroyed', :model => I18n.t('activerecord.models.product'))
+  end
 end
 
