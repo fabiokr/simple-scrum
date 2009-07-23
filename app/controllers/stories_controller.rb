4b08734b9ffd2543851ea6aa4f8b709f352cbd11
Fabio Kreusch <fabiokr@gmail.com>
2009-07-23 09:51:20 -0300

    Refactored index methods

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -6,15 +6,73 @@ class StoriesController < ApplicationController
     @product = Product.find(params[:product_id])
   end
 
-  # GET /products/1/stories
-  # GET /products/1/stories.json
+  # GET /stories/1/stories
   def index
-    @search = Story.search({:product_id_equals => @product.id}.merge(params[:search] || {}))
+    @search = @product.stories.search(params[:search])
     @stories = @search.all
 
     respond_to do |format|
       format.html # index.html.erb
       format.json  { render :json => @stories }
+    end
+  end
+
+  # GET /stories/new
+  def new
+    @story = Story.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json  { render :json => @story }
+    end
+  end
+
+  # GET /stories/1/edit
+  def edit
+    @story = Story.find(params[:id])
+  end
+
+  # POST /stories
+  def create
+    @story = Story.new(params[:product])
+
+    respond_to do |format|
+      if @story.save
+        flash[:message] = t('system.successfully_created', :model => t('activerecord.models.product'))
+        format.html { redirect_to(product_url(@story)) }
+        format.json  { render :json => @story, :status => :created, :location => @story }
+      else
+        format.html { render :action => "new" }
+        format.json  { render :json => @story.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /stories/1
+  def update
+    @story = Story.find(params[:id])
+
+    respond_to do |format|
+      if @story.update_attributes(params[:product])
+        flash[:message] = t('system.successfully_updated', :model => t('activerecord.models.product'))
+        format.html { redirect_to(product_url(@story)) }
+        format.json  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.json  { render :json => @story.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /stories/1
+  def destroy
+    @story = Story.find(params[:id])
+    @story.destroy
+
+    respond_to do |format|
+      flash[:message] = t('system.successfully_destroyed', :model => t('activerecord.models.product'))
+      format.html { redirect_to(products_url) }
+      format.json  { head :ok }
     end
   end
 
--- test/functional/stories_controller_test.rb
+++ test/functional/stories_controller_test.rb
@@ -2,24 +2,29 @@ require 'test_helper'
 
 class StoriesControllerTest < ActionController::TestCase
 
-  context 'on GET to :index' do
+  context 'A request' do
     setup do
       @product = Factory(:product)
+      #expectation for views that uses Product.all to list all products
       @products = [@product, Factory(:product)].paginate
-      @stories = [Factory(:story), Factory(:story)].paginate
-
-      @search = Story.search
-
-      @search.stubs(:all => @stories)
-      Story.expects(:search => @search)
       Product.expects(:find => @product, :all => @products)
-
-      get :index, :product_id => @product.id
     end
 
-    should_respond_with :success
-    should_assign_to(:stories) {@stories}
-    should_assign_to(:product) {@product}
+    context 'on GET to :index' do
+      setup do
+        @stories = [Factory(:story), Factory(:story)].paginate
+        @product.expects(:stories => @stories)
+        @search = Story.search
+        @stories.expects(:search => @search)
+        @search.expects(:all => @stories)
+
+        get :index, :product_id => @product.id
+      end
+
+      should_respond_with :success
+      should_assign_to(:stories) {@stories}
+      should_assign_to(:product) {@product}
+    end
   end
 end
 
