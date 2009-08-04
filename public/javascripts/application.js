6bd160b74c85c67daf0aeb208e615f4810036b93
Fabio Kreusch <fabiokr@gmail.com>
2009-08-04 10:04:07 -0300

    Added ajax to pagination

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
-
+  //adds ajax to pagination links
+  $('div.pagination a').live('click', function(){
+    $('#inner-content').spin()
+      .load($(this)
+      .attr('href'), null, function(){
+        $.Spinner.unspin();
+      })
+    return false;
+  });
 });
 
--- public/javascripts/products/index.js
+++ public/javascripts/products/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('table#dataList tbody tr').click(function() {
+  $('table#dataList tbody tr').live('click', function(){
      window.location = $(this).find('a.showLink').attr('href');
   });
 });
