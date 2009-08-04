e31c8019b2bbf4d7c30a3e467340fd603a698e9f
Fabio Kreusch <fabiokr@gmail.com>
2009-08-04 11:06:22 -0300

    Fixed live problem with links

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -14,13 +14,24 @@ function addAjaxToForm() {
   $.Spinner.unspin();
 }
 
+function addAjaxToDestroyForm() {
+  $('form.deleteLink').ajaxForm({target:'#inner-content', success: addAjaxToForm});
+}
+
 function addAjaxToDataTable() {
   $('table#dataList tbody tr').live('click', function(){
     $('#inner-content').spin()
       .load($(this)
       .find('a.editLink')
       .attr('href'), null, addAjaxToForm);
-    return false;
+      return false;
+  });
+
+  $('table#dataList tbody tr td a').live('click', function(){
+    $('#inner-content').spin()
+      .load($(this)
+      .attr('href'), null, addAjaxToForm);
+      return false;
   });
 }
 
