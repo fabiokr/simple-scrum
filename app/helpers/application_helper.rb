03ed5ca414e7a501b75abf6b2daf10e389519cbd
Fabio Kreusch <fabiokr@gmail.com>
2009-08-09 12:26:55 -0300

    Updated with W3C validation corrections

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,15 +12,15 @@ module ApplicationHelper
   end
 
   def new_link(path, text)
-    link_to "#{icon(:page_add)}#{text}", path, :class => 'button newLink'
+    link_to "#{icon(:page_add, :alt => text)}#{text}", path, :class => 'button newLink'
   end
 
   def show_link(path)
-    link_to icon(:page), path, :alt => t('system.show'), :title => t('system.show'), :class => 'showLink'
+    link_to icon(:page, :alt => t('system.show')), path, :title => t('system.show'), :class => 'showLink'
   end
 
   def edit_link(path)
-    link_to icon(:page_edit), path, :alt => t('system.edit'), :title => t('system.edit'), :class => 'editLink'
+    link_to icon(:page_edit, :alt => t('system.edit')), path, :title => t('system.edit'), :class => 'editLink'
   end
 
   def delete_link(path)
@@ -28,12 +28,12 @@ module ApplicationHelper
 
     form = content_tag('form', :action => html_options['action'], :method => 'post', :class => 'deleteLink') do
       content = ''
-      content << hidden_field_tag('_method', 'delete')
-      content << hidden_field_tag(request_forgery_protection_token.to_s, form_authenticity_token)
+      content << hidden_field_tag('_method', 'delete', :id => nil)
+      content << hidden_field_tag(request_forgery_protection_token.to_s, form_authenticity_token, :id => nil)
       content << image_submit_tag("#{Icons::IMG_SRC}#{Icons.current_set}/page_delete.png", :alt => t('system.destroy'), :title => t('system.destroy'))
     end
 
-    form << link_to(icon(:page_delete), path, :alt => t('system.destroy'), :title => t('system.destroy'), :class => 'deleteLink hide')
+    form << link_to(icon(:page_delete, :alt => t('system.destroy')), path, :title => t('system.destroy'), :class => 'deleteLink hide')
   end
 
 end
