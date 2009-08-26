041859aab5c195c01ff8ff186d4e45ffb8a6a5d5
Fabio Kreusch <fabiokr@gmail.com>
2009-08-26 10:42:06 -0300

    Reordered gems

--- config/environment.rb
+++ config/environment.rb
@@ -18,20 +18,20 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
-  #config.gem 'thoughtbot-shoulda', :lib => 'shoulda/rails', :source => "http://gems.github.com"
-  config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => "http://gems.github.com"
-  config.gem 'haml'
-  config.gem 'jscruggs-metric_fu', :lib => 'metric_fu', :source => 'http://gems.github.com'
-  config.gem 'random_data'
-  config.gem 'webrat', :source => 'http://gems.github.com'
-  config.gem "searchlogic"
-  config.gem 'mocha'
-  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
-  config.gem 'json'
+
   config.gem "rspec", :lib => false, :version => ">= 1.2.0"
   config.gem "rspec-rails", :lib => false, :version => ">= 1.2"
   config.gem "remarkable_rails", :lib => false
+  config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => "http://gems.github.com"
+  config.gem 'webrat', :source => 'http://gems.github.com'
+
+  config.gem 'haml'
+  config.gem 'jscruggs-metric_fu', :lib => 'metric_fu', :source => 'http://gems.github.com'
+  config.gem 'random_data'
+  config.gem "searchlogic"
+  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem 'json'
+
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
