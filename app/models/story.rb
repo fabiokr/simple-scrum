4955f0a1b5d272e77bee6730144af36cd54ca791
Fabio Kreusch <fabiokr@gmail.com>
2009-08-28 09:17:34 -0300

    Changed validation to lenght only

--- app/models/story.rb
+++ app/models/story.rb
@@ -16,7 +16,7 @@ class Story < ActiveRecord::Base
 
   belongs_to :product
 
-  validates_presence_of :name, :product_id
+  validates_presence_of :product_id
   validates_length_of :name, :in => 1..200
 
   validates_numericality_of :estimative
