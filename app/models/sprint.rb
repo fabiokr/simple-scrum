0feb04d4d3e08bb93e5b5312a1238b0bbde6701b
Fabio Kreusch <fabiokr@gmail.com>
2009-08-31 11:17:05 -0300

    Added i18n for Sprints

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -17,6 +17,6 @@ class Sprint < ActiveRecord::Base
 
   validates_presence_of :product_id
   validates_length_of :name, :in => 1..60
-  validates_numericality_of :velocity
+  validates_numericality_of :velocity, :allow_nil => true
 end
 
--- config/initializers/breadcumbs.rb
+++ config/initializers/breadcumbs.rb
@@ -11,6 +11,11 @@ Breadcrumb.configure do
   crumb :story_new, '#{t("app.stories.new")}', :new_product_story_url, :product
   crumb :story_edit, '#{t("app.stories.edit")}', :edit_product_story_url, :product, :story
 
+  crumb :sprint_index, '#{t("app.sprints.index")}', :product_sprints_url, :product
+  crumb :sprint_show, '#{t("app.sprints.show")}', :product_sprint_url, :product, :sprint
+  crumb :sprint_new, '#{t("app.sprints.new")}', :new_product_sprint_url, :product
+  crumb :sprint_edit, '#{t("app.sprints.edit")}', :edit_product_sprint_url, :product, :sprint
+
   #Trails
 
   root = [:product_index]
@@ -23,12 +28,21 @@ Breadcrumb.configure do
   end
 
   context 'stories controller' do
-    backlog = root + [:product_show, :story_index]
+    controller_root = root + [:product_show, :story_index]
 
-    trail :stories, :index, backlog
-    trail :stories, :show,  backlog + [:story_show]
-    trail :stories, :new,   backlog + [:story_new]
-    trail :stories, :edit,  backlog + [:story_edit]
+    trail :stories, :index, controller_root
+    trail :stories, :show,  controller_root + [:story_show]
+    trail :stories, :new,   controller_root + [:story_new]
+    trail :stories, :edit,  controller_root + [:story_edit]
+  end
+
+  context 'sprints controller' do
+    controller_root = root + [:product_show, :sprint_index]
+
+    trail :sprints, :index, controller_root
+    trail :sprints, :show,  controller_root + [:sprint_show]
+    trail :sprints, :new,   controller_root + [:sprint_new]
+    trail :sprints, :edit,  controller_root + [:sprint_edit]
   end
 
   delimit_with "|"
