a2f789e6711b92ace8711e6474e7500160dabaf4
Fabio Kreusch <fabiokr@gmail.com>
2009-09-05 20:27:40 -0300

    Refactored spec

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,6 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ProductsController do
+
+  integrate_views
+
   should_route :get, '/products', :controller => :products, :action => :index
   should_route :get, '/products/1', :controller => :products, :action => :show, :id => 1
   should_route :get, '/products/new', :controller => :products, :action => :new
@@ -8,139 +11,76 @@ describe ProductsController do
   should_route :post, '/products', :controller => :products, :action => :create
   should_route :put, '/products/1', :controller => :products, :action => :update, :id => 1
   should_route :delete, '/products/1', :controller => :products, :action => :destroy, :id => 1
-end
 
-describe ProductsController, 'on index' do
-
-  integrate_views
-
-  it "should list products" do
+  it "should list products on :index" do
+    @product = Factory(:product, :name => 'test')
     get :index, :page => 1, :search => {:name_like => 'test'}
 
-    assigns(:search)
-    assigns(:products)
+    assigns(:search).should_not be_nil
+    assigns(:products).should include(@product)
   end
 
-end
-
-describe ProductsController, 'on show' do
-
-  integrate_views
-
-  it "should show product" do
-    @product = Factory.build(:product)
-    Product.should_receive(:find).with(@product.id.to_s).and_return(@product)
-
+  it "should assign product on :show" do
+    @product = Factory(:product)
     get :show, :id => @product.id
 
-    assert_equal @product, assigns(:product)
+    assigns(:product).should == @product
   end
 
-end
-
-describe ProductsController, 'on new' do
-
-  integrate_views
-
-  it "should edit new product" do
-    @product = Factory.build(:product)
-    Product.should_receive(:new).and_return(@product)
-
+  it "should assign a new product on :new" do
     get :new
 
-    assert_equal @product, assigns(:product)
+    assigns(:product).should be_new_record
   end
 
-end
-
-describe ProductsController, 'on edit' do
-
-  integrate_views
-
-  it "should edit existing product" do
-    @product = Factory.build(:product)
-    Product.should_receive(:find).with(@product.id.to_s).and_return(@product)
-
+  it "should assign product on :edit" do
+    @product = Factory(:product)
     get :edit, :id => @product.id
 
-    assert_equal @product, assigns(:product)
+    assigns(:product).should == @product
   end
 
-end
-
-describe ProductsController, 'on create' do
-
-  integrate_views
-
-  before(:each) do
+  it "should add new product on :create" do
     @product = Factory.build(:product)
-    Product.should_receive(:new).with(@product.attributes).and_return(@product)
-  end
-
-  it "should redirect to index with notice when valid" do
-    @product.should_receive(:save).and_return(true)
-
     post :create, :product => @product.attributes
 
-    assert_equal @product, assigns(:product)
-    flash[:message].should_not be_nil
+    assigns(:product).should == Product.find(assigns(:product).id)
     response.should redirect_to(products_path)
+    flash[:message].should_not be_nil
   end
 
-  it "should re-render new when invalid" do
-    @product.should_receive(:save).and_return(false)
-
+  it "should re-render new when invalid on :create" do
+    @product = Factory.build(:product, :id => nil, :name => nil)
     post :create, :product => @product.attributes
 
-    assert_equal @product, assigns(:product)
     response.should render_template('new')
   end
 
-end
-
-describe ProductsController, 'on update' do
-
-  integrate_views
-
-  before(:each) do
-    @product = Factory.build(:product)
-    Product.should_receive(:find).with(@product.id.to_s).and_return(@product)
-  end
-
-  it "should redirect to index with notice when valid" do
-    @product.should_receive(:update_attributes).and_return(true)
-
+  it "should save product on :update" do
+    @product = Factory(:product)
+    @product.name = 'new name'
     post :update, :id => @product.id, :product => @product.attributes
 
-    assert_equal @product, assigns(:product)
-    flash[:message].should_not be_nil
+    Product.find(assigns(:product).id).name.should == @product.name
     response.should redirect_to(products_path)
+    flash[:message].should_not be_nil
   end
 
-  it "should re-render edit when invalid" do
-    @product.should_receive(:update_attributes).and_return(false)
-
+  it "should re-render edit when invalid on :update" do
+    @product = Factory(:product)
+    @product.name = nil
     post :update, :id => @product.id, :product => @product.attributes
 
-    assert_equal @product, assigns(:product)
     response.should render_template('edit')
   end
 
-end
-
-describe ProductsController, 'on destroy' do
-
-  integrate_views
-
-  it "should redirect to index" do
-    @product = Factory.build(:product)
-    Product.should_receive(:find).with(@product.id.to_s).and_return(@product)
-    @product.should_receive(:destroy).and_return(true)
-
+  it "should delete product on :destroy" do
+    @product = Factory(:product)
     get :destroy, :id => @product.id
 
-    flash[:message].should_not be_nil
+    lambda { Product.find(@product.id) }.should raise_error
     response.should redirect_to(products_path)
+    flash[:message].should_not be_nil
   end
 
 end
