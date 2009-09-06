76306c6fb6ffc85231274612921c583b503bacc1
Fabio Kreusch <fabiokr@gmail.com>
2009-09-06 14:37:25 -0300

    Added sprint seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,9 @@
-#generate products
 products = []
 25.times { products << Factory(:product)}
 
-#generate stories
 stories = []
 200.times { stories << Factory(:story, :product => products.rand)}
 
-# results
-puts "#{products.size} products created"
-puts "#{stories.size} stories created"
+sprints = []
+30.times { sprints << Factory(:sprint, :product => products.rand) }
 
