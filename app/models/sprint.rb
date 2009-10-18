f97ef7414d79662951a05df546552dd3a715eb58
Fabio Kreusch <fabiokr@gmail.com>
2009-10-18 08:28:17 -0200

    Refactored Sprint model

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -39,29 +39,14 @@ class Sprint < ActiveRecord::Base
   end
 
   def burndown_plot
-    plot = {}
-    plot[:expected] = {}
-    plot[:current] = {}
-    plot[:labels] = {}
-
-    plot[:expected][:x] = []
-    plot[:expected][:y] = []
-    plot[:current][:x] = []
-    plot[:current][:y] = []
-    plot[:labels][:x] = []
-    plot[:labels][:y] = []
+    plot = {:expected => {:x => [], :y => []}, :current => {:x => [], :y => []}, :labels => {:x => [], :y => []}}
 
     return plot if self.start.nil? || self.end.nil? || self.tasks.empty?
 
     result = calculate_current
-    plot[:current][:y] = result[:y]
-    plot[:current][:x] = result[:x]
-
-    plot[:labels][:x] = distribute_labels(result[:x_labels], 15.0)
-    plot[:labels][:y] = distribute_labels((0..self.estimated_velocity).to_a, 20.0)
-
-    plot[:expected][:y] = [100, 0]
-    plot[:expected][:x] = [0, 100]
+    plot[:current][:x], plot[:current][:y] = result[:x], result[:y]
+    plot[:labels][:x], plot[:labels][:y] = distribute_labels(result[:x_labels], 15.0), distribute_labels((0..self.estimated_velocity).to_a, 20.0)
+    plot[:expected][:x], plot[:expected][:y] = [0, 100], [100, 0]
 
     plot
   end
@@ -83,9 +68,33 @@ class Sprint < ActiveRecord::Base
 
   #plot methods
   def calculate_current
-    y, x, x_labels, x_value, today, current_date, points, stories_end = [], [], [], 0, Date.current, self.start, self.estimated_velocity, {}
+    y, x, x_labels, x_value, today, points, stories_end = [], [], [], 0, Date.current, self.estimated_velocity, calculate_stories_end
 
-    #organizes the dates that the stories ended
+    weekdays = (self.start..self.end).find_all {|date| date.weekday?}
+    weekdays.each do |date|
+      x_labels << date.strftime(PLOT_DATE_FORMAT)
+      if date <= today
+        x << x_value
+        x_value += 1
+        stories_end[date].each {|story| points -= story.estimative} if stories_end.has_key? date
+        y << points
+      end
+    end
+
+    #distribute the data arround the max value of 100
+    factor_x, factor_y  = (100.0/x_labels.size).round_with_precision(1), (100.0/y.max).round_with_precision(1)
+
+    x.collect! {|v| BigDecimal.new((v*factor_x).to_s) }
+    y.collect! {|v| BigDecimal.new((v*factor_y).to_s) }
+
+    {:x => x, :y => y, :x_labels => x_labels}
+  end
+
+  private
+
+  #organizes the dates that the stories ended
+  def calculate_stories_end
+    stories_end = {}
     self.group_tasks_by_story.each do |story, tasks|
       story_ended = true
       tasks.each {|task| story_ended = false if task.status != Taskk::DONE}
@@ -96,27 +105,7 @@ class Sprint < ActiveRecord::Base
         stories_end[date] << story
       end
     end
-
-    until current_date > self.end
-      if current_date.weekday?
-        x_labels << current_date.strftime(PLOT_DATE_FORMAT)
-        if current_date <= today
-          x << x_value
-          x_value += 1
-          stories_end[current_date].each {|story| points -= story.estimative} if stories_end.has_key? current_date
-          y << points
-        end
-      end
-      current_date = current_date.next
-    end
-
-    #distribute the data arround the max value of 100
-    factor_x, factor_y  = (100.0/x_labels.size).round_with_precision(1), (100.0/y.max).round_with_precision(1)
-
-    x.collect! {|v| BigDecimal.new((v*factor_x).to_s) }
-    y.collect! {|v| BigDecimal.new((v*factor_y).to_s) }
-
-    {:x => x, :y => y, :x_labels => x_labels}
+    stories_end
   end
 
   def distribute_labels(values, max = 20.0)
