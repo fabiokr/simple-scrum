706d4550c8cd0dc0a4080d5f303b8ac570d88675
Fabio <fabio@fkreusch.(none)>
2009-11-23 10:54:06 -0200

    Added pt-BR

---
+++ config/initializers/will_paginate.rb
@@ -0,0 +1,3 @@
+WillPaginate::ViewHelpers.pagination_options[:previous_label] = I18n.t('app.paginate.previous')
+WillPaginate::ViewHelpers.pagination_options[:next_label] = I18n.t('app.paginate.next')
+
