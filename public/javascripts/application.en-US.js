82c41f46d7e67353a3954c10d3e15f95d60008a7
Fabio <fabio@fkreusch.(none)>
2009-12-04 10:59:55 -0200

    Removed trailling comma

--- public/javascripts/application.en-US.js
+++ public/javascripts/application.en-US.js
@@ -1,5 +1,5 @@
 var i18n = {
   confirm_destroy: 'Are you sure?',
-  product_backlog_dialog_title: 'Details',
+  product_backlog_dialog_title: 'Details'
 }
 
--- public/javascripts/application.pt-BR.js
+++ public/javascripts/application.pt-BR.js
@@ -1,5 +1,5 @@
 var i18n = {
   confirm_destroy: 'Tem certeza?',
-  product_backlog_dialog_title: 'Detalhes',
+  product_backlog_dialog_title: 'Detalhes'
 }
 
