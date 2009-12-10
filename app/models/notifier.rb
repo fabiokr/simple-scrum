5f6718ec8ab2bce223d4c7d5e828366cfddd1640
Fabio <fabio@fkreusch.(none)>
2009-12-10 10:36:28 -0200

    Moved EMAIL_FROM to constants

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -4,7 +4,7 @@ class Notifier < ActionMailer::Base
 
   def password_reset_instructions(user)
     subject I18n.t('app.password_resets.email_subject')
-    from I18n.t('app.password_resets.email_from')
+    from EMAIL_FROM
     recipients user.email
     sent_on Time.now
     body :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,2 +1,3 @@
 SITE_HOST = "localhost:3000"
+EMAIL_FROM = "Simple Scrum"
 
