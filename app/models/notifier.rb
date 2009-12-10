4234866473eba15be59614fa45907713d1b1f87a
Fabio <fabio@fkreusch.(none)>
2009-12-10 10:31:54 -0200

    Fixes for email

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -8,6 +8,7 @@ class Notifier < ActionMailer::Base
     recipients user.email
     sent_on Time.now
     body :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
+    content_type  "text/html"
   end
 end
 
