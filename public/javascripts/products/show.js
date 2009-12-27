9d9897a78fb36b959b62cf2c56d6e299ae07dda7
Fabio <fabio@fkreusch.(none)>
2009-12-27 10:37:52 -0200

    Fixed Sprint show javascript

--- public/javascripts/products/show.js
+++ public/javascripts/products/show.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('#tickets table.dataList tbody tr td:not(:has(*))').live('click', function(){
     showDetailDialog($(this).parent().find('a.showLink').attr('href'));
   });
-  $('#sprints table.dataList tbody tr td:not(:has(*))').live('click', function(){
+  $('#sprints table.dataList caption').live('click', function(){
     window.location = $(this).parent().find('a.showLink').attr('href');
   });
 });
