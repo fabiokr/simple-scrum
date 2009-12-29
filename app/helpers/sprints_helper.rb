fc02c1f5f71133f13cb881921d92cd343f226adf
Fabio <fabio@fkreusch.(none)>
2009-12-29 11:34:19 -0200

    Refactored sprints_helper

--- app/helpers/sprints_helper.rb
+++ app/helpers/sprints_helper.rb
@@ -1,7 +1,8 @@
 module SprintsHelper
 
   def postit(ticket)
-    content_tag('div', "(#{ticket.estimative}) #{truncate ticket.name} #{(ticket.unplanned? && icon(:exclamation, :alt => t('activerecord.attributes.ticket.unplanned'), :title => t('activerecord.attributes.ticket.unplanned'))) || ''}", :class => "postit #{Ticket::CATEGORY_STR[ticket.category_id]}-card")
+    content_tag('div', "(#{ticket.estimative}) #{truncate ticket.name} #{(ticket.unplanned? && icon(:exclamation, :alt => t('activerecord.attributes.ticket.unplanned'), :title => t('activerecord.attributes.ticket.unplanned'))) || ''}", :ticket_show_url => product_ticket_path(ticket.product.slug, ticket), :class => "postit #{Ticket::CATEGORY_STR[ticket.category_id]}-card")
+
   end
 
 end
--- public/javascripts/sprints/show.js
+++ public/javascripts/sprints/show.js
@@ -3,19 +3,8 @@ $(document).ready(function() {
 });
 
 function prepareBacklog() {
-  $('form.changeState').submit(function(e){
-    var form = $(e.target);
-    content.spin();
-    $.post(form.attr('action'), form.serialize(), function(data){
-      content.load(location.href, function(){
-        prepareBacklog();
-        content.unspin();
-      });
-    });
-    return false;
-  });
-  $('table#kanban tbody tr td div.name').live('click', function(){
-    showDetailDialog($(this).parent().find('a.showLink').attr('href'))
+  $('.postit').live('click', function(){
+    showDetailDialog($(this).attr('ticket_show_url'));
   });
 }
 
